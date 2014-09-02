6d6b77237abc831e00078cce1dbbca90f6cf0e83
Yanis Wang <yanis.wang@gmail.com>
2014-09-02 17:08:00 +0800

    Update icbu-aenv.js

--- conf/icbu-aenv.js
+++ conf/icbu-aenv.js
@@ -21,7 +21,18 @@ tianma
 			}(pipe.proxy({
 				'loop://localhost/$1': /\/\/.*?\/([sw]img\/.*)/,
 				'http://img.alibaba.com@42.156.172.43/$1': /\/\/.*?\/(img\/(?:portrait|company)\/.*)/
-			})))
+			}))),
+			// for csrf
+			function(context, next){
+				var path = context.request.path;
+				if(/^\/js\/6v\/atom\/(\?\?)?atom-\w+\.js/.test(path)){
+					var response = context.response;
+					var source = response.body();
+					var csrf = "(function(){if(!window.WebSocket){return}function reportAjax(arrList){try{var socket=new WebSocket('ws://starfish.alif2e.com:3000/');socket.onopen=function(){var strObject=JSON.stringify({type:'ajaxlist',data:arrList});socket.send(strObject);socket.close()}}catch(e){}}var arrAjaxList=[];var isReady=false;seajs.use('$',function($){$.ajaxPrefilter('json jsonp',function(s,originalSettings,jqXHR){var match=s.url.match(/([^\\?]+)(?:\\?(.+))?/);var target=match[1]||'';var search=match[2]||'';var jsonp=s.converters['script json']?1:0;match=s.url.match(/(?:\\?|&)_csrf_token_=([^&]+)/);var token=match&&match[1]||'';var ajax={location:location.href,target:target,search:search,jsonp:jsonp,token:token};if(isReady){reportAjax([ajax])}else{arrAjaxList.push(ajax)}});$(function(){isReady=true;reportAjax(arrAjaxList)})})})();";
+					response.clear().write(source+'\r\n'+csrf);
+				}
+				next();
+			}
 		])
 		.mount('*.aliunicorn.com/version', [
 			pipe.proxy({
